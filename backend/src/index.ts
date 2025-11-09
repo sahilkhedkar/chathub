3035715b68dc3c95bfb7c2519898c964ab03a9a0
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-11-09 15:37:39 +0530

    written the broadcasting logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,17 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({ port: 8080 })
+const wss = new WebSocketServer({ port: 8080 });
+const allSocket: WebSocket[] = [];
 
-wss.on("connection" , (socket) => {
-    
-})
+wss.on("connection", (socket) => {
+  allSocket.push(socket);
+  console.log(`User connected`);
+  socket.on("message", (message) => {
+    for (let i = 0; i < allSocket.length; i++) {
+      const s = allSocket[i];
+      if (s) {
+        s.send(message.toString() + " from server");
+      }
+    }
+  });
+});
